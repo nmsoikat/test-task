a7504bacc1fe4be554d21f6806b6d311e1a86e39
unknown <nursaykat@gmail.com>
2021-02-19 18:31:43 +0600

    inital commit

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+window.onload = function () {
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 15,
+    nav: true,
+    dots: false,
+    navText: [
+      '<i class="fas fa-angle-left"></i>',
+      '<i class="fas fa-angle-right"></i>',
+    ],
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+      },
+      576: {
+        items: 2,
+      },
+      768: {
+        items: 3,
+      },
+      992: {
+        items: 4,
+      },
+      1200: {
+        items: 5,
+      },
+    },
+  })
+}
